5ad0fa47ddd6985271b85f4e4b1268413d1efd75
whwoohw <woojoo1114@naver.com>
2023-04-09 13:58:38 +0900

    commit before pull

--- chatpt_server/settings.py
+++ chatpt_server/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SECURE_SSL_REDIRECT = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","chatpt-server.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","chatpt-server.onrender.com", "whwoohw.pythonanywhere.com"]
 
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
